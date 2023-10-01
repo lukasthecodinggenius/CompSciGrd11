4b5543b6ac148747effcf6dc93df4779f650565a
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-09-30 21:18:44 -0400

    Create greater-of-two-numbers.java

---
+++ Java/September and October/In Class Work Assignment/greater-of-two-numbers.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+int a = 5, b = 6;
+
+if (a > b) {
+  System.out.println(a + " is bigger than " + b);
+} else {
+  System.out.println(b + " is bigger than " + a);
+}
+
+  }
+}
