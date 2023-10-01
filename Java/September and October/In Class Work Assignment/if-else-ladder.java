cf6d4a12be17d75cb22d5656a8cb2b451bfaae65
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-09-30 21:28:37 -0400

    Update and rename if-else-ladder to if-else-ladder.java

---
+++ Java/September and October/In Class Work Assignment/if-else-ladder.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+  
+  int s = 49;  // declare variable
+
+    if(s > 50) {
+      System.out.println("You passed the test!"); //prints this statement if S is greater than 50
+        } 
+     else if (s < 50) {
+          System.out.println("You failed the test :("); //prints this statement if s is less than 50
+     }
+    
+      
+      
+  } 
+
+  }
