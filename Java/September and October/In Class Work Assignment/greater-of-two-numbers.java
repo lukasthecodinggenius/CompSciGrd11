1ab1b2aec3b1dc462ab40cef8d47c167269e8a94
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-09-30 21:20:52 -0400

    Update greater-of-two-numbers.java

--- Java/September and October/In Class Work Assignment/greater-of-two-numbers.java
+++ Java/September and October/In Class Work Assignment/greater-of-two-numbers.java
@@ -1,12 +1,12 @@
 class Main {
   public static void main(String[] args) {
 
-int a = 5, b = 6;
+int a = 5, b = 6; // declare variables
 
 if (a > b) {
-  System.out.println(a + " is bigger than " + b);
+  System.out.println(a + " is bigger than " + b); //prints this statement if a is a larger number than b
 } else {
-  System.out.println(b + " is bigger than " + a);
+  System.out.println(b + " is bigger than " + a); //prints this statement if b is larger than a
 }
 
   }
