eab07ddecb1c11e5b0d3210d7d5993eadd4c258a
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-09-30 21:34:43 -0400

    Create compound-operators.java

---
+++ Java/September and October/In Class Work Assignment/compound-operators.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+  
+  int a = 10;
+  int b = 12; //declaring the variables.
+
+  System.out.println(a += 10); //same as a = a + 10
+  System.out.println(b *= 10); //same as b = b x 10
+
+
+    
+      
+      
+  } 
+
+  }
+
