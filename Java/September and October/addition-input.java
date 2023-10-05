b0538a1e460d94635d205cb6c65a4806c6d8e3b7
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-05 13:45:01 -0400

    Create addition-input.java

---
+++ Java/September and October/addition-input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  
+Scanner value = new Scanner(System.in);
+System.out.println("Enter numbers: ");
+
+int num1 = value.nextInt();
+int num2 = value.nextInt();
+
+System.out.println("Number is " + (num1 + num2));
+
+  } 
+
+  }
