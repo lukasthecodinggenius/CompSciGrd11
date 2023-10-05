dc58833facd55c5090766e16c16b69308b469065
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-05 13:01:47 -0400

    Create grade-calculator.java

---
+++ Java/September and October/grade-calculator.java
@@ -0,0 +1,34 @@
+class Main {
+  public static void main(String[] args) {
+  
+int grade = 99;
+
+if (grade == 100) {
+  System.out.println("Student got perfect");
+
+} else if (grade >= 85) {
+
+  System.out.println("Student got an A");
+
+} else if (grade >=70) {
+
+  System.out.println("Student got a B");
+
+} else if (grade >= 60) {
+
+  System.out.println("Student got a C");
+
+} else if (grade >= 50) {
+  
+  System.out.println("Student got a D");
+
+} else {
+
+  System.out.println("Student failed");
+}
+      
+      
+  } 
+
+  }
+
