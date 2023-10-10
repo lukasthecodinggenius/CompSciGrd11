465911d8c292d1833a70c13885dab490e647e6e8
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-10 13:05:43 -0400

    Create gradecalculator.java

---
+++ Java/September and October/gradecalculator.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+  
+int grade = 40;
+
+
+ if (grade >= 80) {
+
+  System.out.println("More than 80%");
+
+} else if (grade >=60) {
+
+  System.out.println("more than 60%"); 
+
+} else if ((grade >= 40) && (grade <= 60)) {
+  
+  System.out.println("Average");
+
+} else {
+
+  System.out.println("Failed");
+}
+      
+      
+  } 
+
+  }
