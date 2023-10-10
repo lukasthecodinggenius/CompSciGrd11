0350bf80f8ededa4b0879a520a9bede0ac5c1769
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-10 13:39:20 -0400

    Create A1.java

---
+++ Java/September and October/A1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  
+Scanner value = new Scanner(System.in);
+
+System.out.println("Enter Name: ");
+
+String name = value.nextLine();
+
+System.out.println("Enter 2nd Name: ");
+
+String name2 = value.nextLine();
+
+if (name.equals(name2)) {
+  System.out.println("Both names are the same!");
+} else  {
+  System.out.println("The names are different");
+}
+
+
