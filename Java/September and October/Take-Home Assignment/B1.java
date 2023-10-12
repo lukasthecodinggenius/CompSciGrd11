d622811e623b1e17bb30352199546778318a756f
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-12 08:32:32 -0400

    Create B1.java

---
+++ Java/September and October/Take-Home Assignment/B1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+Scanner value = new Scanner(System.in);
+
+System.out.println("What Country are you from? Type your answer below: ");
+
+String country = value.nextLine();
+
+if (country == "Canada") {
+
+  System.out.println("What is on Canada's Flag? a) Poutine b) A Beaver c) Maple Leaf d) Maple Syrup. Type a, b, c or d");
+    String trivia1 = value.nextLine();
+
+  if (trivia1 == "c") {
+    System.out.println("Correct! You know a lot about your country");
+    System.exit(1)
+  } else {
+    System.out.println("Incorrect, you should know more about your country");
+    System.exit(1);
+  }
+
+} else if (country == "")
+
+
+
+
+  } 
+
+  }
