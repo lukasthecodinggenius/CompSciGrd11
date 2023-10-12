d1922b6ffc24d9bfaeb4ed6c2d8c5e3d2794b2ee
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-12 13:13:08 -0400

    Update A3.java

--- Java/September and October/Take-Home Assignment/A3.java
+++ Java/September and October/Take-Home Assignment/A3.java
@@ -3,37 +3,37 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-Scanner value = new Scanner(System.in);
+Scanner value = new Scanner(System.in); //declare scanner
 
-System.out.println("Problem 1: 3x3, Answer Below: ");
+System.out.println("Problem 1: 3x3, Answer Below: "); //the first math problem
 
-int math1 = value.nextInt();
+int math1 = value.nextInt(); //take input from the user
 
 if (math1 == 9) {
-  System.out.println("You got it right!");
+  System.out.println("You got it right!"); //if the user gets the answer right, it prints this and moves onto the next question
 } else {
-  System.out.println("Incorrect, Score 0/3, Try Again!");
-  System.exit(1);
+  System.out.println("Incorrect, Score 0/3, Try Again!"); //if the user gets the answer wrong, it prints this
+  System.exit(1); //when the user gets the question wrong, the program gets terminated
 }
 
-System.out.println("Problem 2: 7 x 8, Answer Below");
-int math2 = value.nextInt();
+System.out.println("Problem 2: 7 x 8, Answer Below"); //2nd math problem
+int math2 = value.nextInt(); //take input from the user
 
 if (math2 == 56) {
-  System.out.println("Correct! Get ready for the last question");
+  System.out.println("Correct! Get ready for the last question"); //prints this if the user gets the question right and moves on to the next question
 } else {
-  System.out.println("Incorrect, Score: 1/3, Try Again!");
-  System.exit(1);
+  System.out.println("Incorrect, Score: 1/3, Try Again!"); //print this if the user gets the question wrong
+  System.exit(1); //terminates the program when the user gets the question wrong
 }
 
-System.out.println("Final Problem: 90920138098501 * 0");
+System.out.println("Final Problem: 90920138098501 * 0"); //final problem
 
-int math3 = value.nextInt();
+int math3 = value.nextInt(); //takes input from the user
 
 if (math3 == 0) {
-  System.out.println("Nice! You got all the question's right with a score of 3/3");
+  System.out.println("Nice! You got all the question's right with a score of 3/3"); //prints this if the user gets the question right
 } else {
-  System.out.println("Incorrect, Score 2/3, Try Again!");
+  System.out.println("Incorrect, Score 2/3, Try Again!"); //prints this if the user gets it wrong.
   
 }
 
