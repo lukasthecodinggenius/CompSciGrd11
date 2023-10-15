5d3a239e1a29c65ef10808bffc7ffc5c0a681ec3
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-14 21:53:39 -0400

    Update B1.java

--- Java/September and October/Take-Home Assignment/B1.java
+++ Java/September and October/Take-Home Assignment/B1.java
@@ -9,20 +9,47 @@ System.out.println("What Country are you from? Type your answer below: ");
 
 String country = value.nextLine();
 
-if (country == "Canada") {
+if (country.equals("Canada")) {
 
-  System.out.println("What is on Canada's Flag? a) Poutine b) A Beaver c) Maple Leaf d) Maple Syrup. Type a, b, c or d");
+  System.out.println("What is on Canada's Flag? a) Poutine b) A Beaver c) Maple Leaf d) Maple Syrup | Type a, b, c or d");
     String trivia1 = value.nextLine();
 
-  if (trivia1 == "c") {
+  if (trivia1.equals("c")) {
     System.out.println("Correct! You know a lot about your country");
-    System.exit(1)
+    System.exit(1);
   } else {
     System.out.println("Incorrect, you should know more about your country");
     System.exit(1);
   }
 
-} else if (country == "")
+} else if (country.equals("Germany")) {
+
+   System.out.println("How do you say hello in german?  a) Hallo b) Bonjour c) Hola d) Ciao | Type a, b, c or d");
+    String trivia2 = value.nextLine();
+
+  if (trivia2.equals("a")) {
+    System.out.println("Correct! You know a lot about your country");
+    System.exit(1);
+  } else {
+    System.out.println("Incorrect, you should know more about your country");
+    System.exit(1);
+  }
+  
+} else if (country.equals("Italy")) {
+   
+  System.out.println("What is the tiny country inside italy called? a) Ciaoland b) Vatican City c) Vaticanty d) Canada | Type a, b, c or d");
+    String trivia3 = value.nextLine();
+
+  if (trivia3.equals("b")) {
+    System.out.println("Correct! You know a lot about your country");
+    System.exit(1);
+  } else {
+    System.out.println("Incorrect, you should know more about your country");
+    System.exit(1);
+  }
+} else {
+  System.out.println("Sorry, I do not have that region in my database");
+}
 
 
 
@@ -30,3 +57,4 @@ if (country == "Canada") {
   } 
 
   }
+
