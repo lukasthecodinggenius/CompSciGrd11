da79b57d701ef4fef59458c9afab4647fbdb3687
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-15 00:31:09 -0400

    Update B5.java

--- Java/September and October/Take-Home Assignment/B5.java
+++ Java/September and October/Take-Home Assignment/B5.java
@@ -8,14 +8,14 @@ Scanner value = new Scanner(System.in);
 
 System.out.println("Player 1 Choose: ");
 
-String p1 = value.nextLine();
+String p1 = value.nextLine(); //take input from the user for player 1
 
 System.out.println("Player 2 Choose: ");
 
-String p2 = value.nextLine();
+String p2 = value.nextLine(); //take input from the user for player 2
 
 
-if (p1.equals("Rock") && (p2.equals("Paper"))) {
+if (p1.equals("Rock") && (p2.equals("Paper"))) {      //outputs for all possible combinations
 		System.out.println("Player 2 Wins");
 } else if (p1.equals("Rock") && (p2.equals("Scissors"))) {
 		System.out.println("Player 1 Wins");
@@ -34,7 +34,7 @@ if (p1.equals("Rock") && (p2.equals("Paper"))) {
 } else if (p1.equals("Scissors") && (p2.equals("Scissors"))) {
 		System.out.println("Tiebreaker");
 } else {
-		System.out.println("Invalid Input");
+		System.out.println("Invalid Input"); //outputs this if the user puts an invalid statement
 }
 
 
