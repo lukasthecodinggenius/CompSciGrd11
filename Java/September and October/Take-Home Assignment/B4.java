5dbb69961d800ca019d1d9086527e85e0ee90fd3
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-15 00:32:55 -0400

    Update B4.java

--- Java/September and October/Take-Home Assignment/B4.java
+++ Java/September and October/Take-Home Assignment/B4.java
@@ -8,10 +8,10 @@ Scanner value = new Scanner(System.in);
 
 System.out.println("Enter Age:");
 
-int age = value.nextInt();
+int age = value.nextInt();  //take input from the user
 
 if (age <= 13) {
-    System.out.println("You are a child");
+    System.out.println("You are a child");        //outputs for differents ages
 } else if ((age <= 19) && (age >= 13)) {
     System.out.println("You are a teen");
 } else if ((age <= 65) && (age >= 20)) {
@@ -19,7 +19,7 @@ if (age <= 13) {
 } else if (age >= 65) {
     System.out.println("You are a senior citizen");
 } else {
-    System.out.println("Age Invalid");
+    System.out.println("Age Invalid"); //outputs this statement if the user puts invalid term
 }
 
 
