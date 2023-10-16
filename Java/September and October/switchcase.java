4e7278d0b0fce95178fc8b33fe75db89f71757de
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-16 13:54:24 -0400

    Create switchcase.java

---
+++ Java/September and October/switchcase.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; //initialize scanner
+
+class Main {
+  public static void main(String[] args) {
+
+Scanner value = new Scanner(System.in);
+
+   char letter = 'a';
+
+    switch(letter) {
+      case 'a':
+      System.out.println("the letter is a");
+  break;
+      case 'b':
+      System.out.println("The letter is b")
+   break;
+  default: System.out.println("Case other than a or b")
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  } 
+
+  }
