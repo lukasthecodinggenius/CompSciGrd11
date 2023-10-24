5e2aee861f6f512e64724d106d42ec824404eeec
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-24 13:16:12 -0400

    Create adding-numbers-input.java

---
+++ Java/September and October/adding-numbers-input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+
+int sum = 0;
+int num = 0;
+
+System.out.println("Enter the amount of numbers you will add");
+
+int additions = input.nextInt();
+
+do {
+  sum +=
+}
+
+
+
+
+
+
+  } 
+
+  }
