bc6a77b5520c71fcf47e3b882e16ad959776ce66
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-10-26 13:08:19 -0400

    Create text-art-forloop.java

---
+++ Java/September and October/text-art-forloop.java
@@ -0,0 +1,17 @@
+
+
+class Main {
+  public static void main(String[] args) {
+
+
+System.out.println("+---------+");
+for(int num1 = 1; num1 <= 3; num1++) {
+  System.out.println("\\        /");
+  System.out.println("/        \\");
+}
+
+System.out.println("+---------+");
+
+
+  } 
+} 
