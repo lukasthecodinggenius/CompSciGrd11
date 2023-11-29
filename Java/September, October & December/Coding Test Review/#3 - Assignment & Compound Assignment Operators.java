50ff265e20e4b6e9b3d7be6a9c4839e62939023e
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-11-29 13:00:38 -0500

    Create #3 - Assignment & Compound Assignment Operators.java

---
+++ Java/September, October & December/Coding Test Review/#3 - Assignment & Compound Assignment Operators.java
@@ -0,0 +1,44 @@
+
+
+class Main {
+
+
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 5; 
+        int num3 = 2; 
+        int n = 1;
+        
+        //simple assignment operators
+        //used in loops (ex. sum of numbers from 1 to 50
+        n = n + 1;
+        num1 = num1 + 1;
+        num2 = num2 - 1;
+        num3 = num3 * (n + 1);
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        
+        //compound assignment operators
+        //simpler way of writing assignment operators
+        
+        int num4 = 100;
+        int num5 = 15; 
+        int num6 = 6;
+        
+        num4 += 1;
+        num5 -= 1;
+        num6 *= (n+1);
+        
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        
+                
+        
+     
+    }
+    
+}
