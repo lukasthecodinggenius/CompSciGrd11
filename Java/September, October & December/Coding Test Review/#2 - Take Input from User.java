9bab2e35288fffd064f976e7eb359ec60ffa804c
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-11-29 11:10:01 -0500

    Create #2 - Take Input from User.java

---
+++ Java/September, October & December/Coding Test Review/#2 - Take Input from User.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+
+class Main {
+  public static void main(String[] args) {
+
+   Scanner scanner1 = new Scanner(System.in);
+
+
+   
+   System.out.println("Enter word: ");
+   //take input for string value (nextLine)
+   String word1 = scanner1.nextLine();
+
+   System.out.println("Enter number: ");
+   //take input for integer value (nextInt)
+   int num1 = scanner1.nextInt();
+
+
+
+   // take input for boolean value = nextBoolean
+  //take input for float value = nextFloat
+  //take input for char value = next().charAt(0)
+  //take input for double value (nextDouble)
+  }
+}   
+
+  
+
+
+
+
