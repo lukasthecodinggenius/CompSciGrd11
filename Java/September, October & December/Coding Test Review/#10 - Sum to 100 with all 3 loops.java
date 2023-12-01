bcdef742f8616885c9c138235b959c2ec0821442
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-12-01 13:42:29 -0500

    Create #10 - Sum to 100 with all 3 loops.java

---
+++ Java/September, October & December/Coding Test Review/#10 - Sum to 100 with all 3 loops.java
@@ -0,0 +1,63 @@
+
+package my_application;
+
+
+class My_Application {
+        
+ 
+    public static void main(String[] args) {
+       
+    
+        //for loop program
+        
+   int sum = 0;
+              
+     for (int n = 0; n < 101; n++) {
+      
+        sum += n;   
+         
+     }
+     System.out.println(sum);
+     
+     
+     
+     //while loop program
+     
+     
+     
+     
+     int sum2 = 0;
+     int i = 0;
+
+     while (i < 100) {
+         i++;
+         sum2 += i;
+         
+     }  
+        System.out.println(sum2);
+        
+        
+      //do while loop program
+      
+      int sum3 = 0;
+      int j = 0;
+      
+      do {
+          j++;
+          sum3 += j;
+      } while (j < 100);
+    
+    System.out.println(sum3);
+     
+     
+     
+              } 
+    }
+    
+
+
+
+
+
+
+
