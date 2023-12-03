856c30679c5278865b73f1fd11a9e8d7c9574730
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-12-02 23:39:22 -0500

    Create #9.5 - Grades using ifelse.java

---
+++ Java/September, October & December/Coding Test Review/#9.5 - Grades using ifelse.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+	Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter Grade: ");
+	int grade = scanner.nextInt();
+
+		if ((grade >= 90) && (grade <= 105)) {
+			System.out.println("You got an A+!");
+		} else if (grade >= 80) {
+			System.out.println("You got an A-!");
+		} else if (grade >= 70) {
+			System.out.println("You got a B!");
+		} else if (grade >= 60) {
+        System.out.println("You got a C!");
+    }  else if (grade >= 50) {
+        System.out.println("You got a D!");
+    }  else if ((grade < 40) && (grade >= 0)) {
+        System.out.println("You failed!");
+		} else {
+			System.out.println("Invalid Grade");
+		}
+	scanner.close(); 
+  }    
+}
