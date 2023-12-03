cc3b7b1163c4cce64ed1d0bf6c994e63675f4a4b
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-12-02 23:30:36 -0500

    Create Extra - Star Pattern.java

---
+++ Java/September, October & December/Coding Test Review/Extra - Star Pattern.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String args[]) {
+
+	    //Write a program to print the following pattern using for loops * ** *** **** ***** ****** ******* ******
+
+		for (int i = 0; i <=5; i++) {
+			
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+
+		
+}
+}
