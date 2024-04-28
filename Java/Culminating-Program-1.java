9c83f5bf4372f4a6e56dc206ae14040ec0e1869f
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-04-28 14:30:35 -0400

    Create Culminating-Program-1.java

---
+++ Java/Culminating-Program-1.java
@@ -0,0 +1,14 @@
+
+package javaapplication2;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        //Printing 'Hello World!'
+        
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
