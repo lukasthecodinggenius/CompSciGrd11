1e4210fa549d98c72385c6b806cbea445f963cd7
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-04-28 14:29:48 -0400

    Create Culminating-Program-2.java

---
+++ Java/Culminating-Program-2.java
@@ -0,0 +1,27 @@
+//Variables Tutorial
+package javaapplication2;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        //Declaring and Initializing Variables
+        
+        //DECLARATION: Datatype VariableName = Value
+        
+        int age = 16; //int for number value
+        double height = 5.8; //double for decimal
+        char gender = 'M'; //char for character value
+        boolean isCool = true; //boolean for a true or false value
+        String name =  "Lukas Muench"; //String for a sequence of characters
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Gender: " + gender);
+        System.out.println("Is Cool: " +  isCool);  
+        
+  
+        
+    }
+    
+}
